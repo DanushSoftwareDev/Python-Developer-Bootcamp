b7e9d5eae4bc3b9abf986bdc2d67cf4e53514f46
Danush <danush.karma.agency@gmail.com>
2024-12-27 21:10:21 +0530

    Add files via upload

--- Day1.py
+++ Day1.py
@@ -1,6 +1,6 @@
 print("Welcome to Band Name Generator")
 # Getting user input
-city=input("What's the name of the city you grew up in?\n") #'\n' represents the newline character
+city=input("Which city did you grow up in?\n") #'\n' represents the newline character
 # By Default all inputs are considered as strings
 pet=input("What's the name of your pet?\n")
 # '+' symbol can be used for string concatenation
