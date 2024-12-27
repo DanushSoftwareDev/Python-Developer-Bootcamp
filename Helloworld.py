064151e1947480b50976b422b8deaf257f5b468f
Danush <danush.karma.agency@gmail.com>
2024-12-27 19:40:13 +0530

    Add files via upload

--- Helloworld.py
+++ Helloworld.py
@@ -1,3 +1,4 @@
 #The classic! My first python program
 print("Hello World!!")
-print("Hi I'm Danush \n This is the start of my programming journey")
+#We can use the newline character to save a few lines of code (UwU)
+print("Hi I'm Danush\nThis is the start of my programming journey")
